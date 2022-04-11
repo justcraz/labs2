05abf971472be4891fa60a9ee6f6c378348db24b
Alexandr Grigoraschuk <64975380+justcraz@users.noreply.github.com>
2022-04-11 18:03:38 +0300

    first commit

---
+++ lab2ex1/lab2ex1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+class program
+{
+    static void Main()
+    {
+        Console.Write("Введiть номер знака зодiака: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        switch (num)
+        {
+            case 1:
+                Console.WriteLine("Овен");
+                break;
+            case 2:
+                Console.WriteLine("Телець");
+                break;
+            case 3:
+                Console.WriteLine("Близнюки");
+                break;
+            case 4:
+                Console.WriteLine("Рак");
+                break;
+            case 5:
+                Console.WriteLine("Лев");
+                break;
+            case 6:
+                Console.WriteLine("Дiва");
+                break;
+            case 7:
+                Console.WriteLine("Терези");
+                break;
+            case 8:
+                Console.WriteLine("Скорпiон");
+                break;
+            case 9:
+                Console.WriteLine("Стрiлець");
+                break;
+            case 10:
+                Console.WriteLine("Козерiг");
+                break;
+            case 11:
+                Console.WriteLine("Водолiй");
+                break;
+            case 12:
+                Console.WriteLine("Риби");
+                break;
+
+            default: Console.WriteLine("Ви ввели неправильну цифру ");
+                break;
+        }
+    }
+}
---
+++ lab2ex1/lab2ex2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+class Type2
+{
+
+    public static void Main()
+    {
+        double a = -Math.PI / 2;
+        double b = 3*Math.PI / 2;
+        double dx = Math.PI / 3;
+        while (true)
+        {
+
+            Console.Write("Виберiть дiю: \n" +
+                    "1 - Обчислiть за допомогою циклу з передумовою \n" +
+                    "2 - Обчислiть за допомогою циклу з пiсляумовою \n" +
+                    "3 - Вийти з програми \n" +
+                    "Ваш вибiр: ");
+            int str = int.Parse(Console.ReadLine());
+            double x, y = 0;
+            if (str == 1)
+            {//Цикл з передумовою
+                x = a;
+                Console.Write("|  x      |   y=f(x)  |\n");
+                while (x <= b)
+                {
+                    Console.Write("|{0:F3}    |    {1,5:F2}  |\n", x, y);
+                    x += dx;
+                    y = Math.Pow(Math.Cos(x), 2) + 3 * Math.Sin(x);
+                }
+            }
+            if (str == 2)
+            {//Цикл з пiсляумовою
+                x = a;
+                Console.Write("|  x      |   y=f(x)  |\n");
+                do
+                {
+                    Console.Write("|{0:F3}    |    {0,5:F2}  |\n", x, y);
+                    x += dx;
+                    y = Math.Pow(Math.Cos(x),2) + 3*Math.Sin(x);
+                } while (x <= b);
+            }
+            if (str == 3)
+            {
+                break;
+            }
+            else
+                if (str < 1 || str > 3)
+                continue;
+        }
+    }
+}
